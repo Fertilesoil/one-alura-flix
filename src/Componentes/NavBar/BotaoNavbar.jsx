156f56fa7f1d9d16e3aac1bf5ea04a76951f0f6f
Fertilesoil <gasnbloop@gmail.com>
2024-06-14 20:34:55 -0300

    Feitura da barra de menu e banner: banner responsivo

---
+++ src/Componentes/NavBar/BotaoNavbar.jsx
@@ -0,0 +1,34 @@
+﻿import styled from 'styled-components'
+
+const BotaoNav = styled.button`
+  font-family: var(--font-source-sans);
+  width: 9rem;
+  border-radius: .4rem;
+  line-height: 2lh;
+  font-size: 1.1rem;
+  font-weight: 600;
+  color: #F7F7F7;
+  background: transparent;
+  border: 2px solid #F7F7F7;
+  cursor: pointer;
+  text-transform: uppercase;
+  transition: all .37s ease-out;
+
+  &:hover {
+    color: var(--standard-blue);
+    box-shadow: var(--blue-shadow-button);
+    border: 2px solid var(--standard-blue);
+    background: var(--dark);
+    transition: all .37s ease-out;
+  }
+`
+
+const BotaoNavbar = ({ children }) => {
+  return (
+    <BotaoNav>
+      {children}
+    </BotaoNav>
+  )
+}
+
+export default BotaoNavbar
---
+++ src/Componentes/NavBar/index.jsx
@@ -0,0 +1,48 @@
+﻿import styled from 'styled-components'
+import logo from "../../assets/logo-alura-flix.png";
+import BotaoNavbar from './BotaoNavbar';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: var(--dark-grey);
+  min-height: 10vh;
+  box-shadow: 0px 5px 29px 0px #2271D1B2;
+
+  & > nav {
+    display: inherit;
+    justify-content: inherit;
+    align-items: inherit;
+    width: 100%;
+  }
+`
+
+const WrapperBotoes = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: inherit;
+  width: fit-content;
+`
+
+const NavBar = () => {
+  return (
+    <Header>
+      <nav>
+        <img src={logo} alt="Logo AluraFlix" />
+
+        <WrapperBotoes>
+          <BotaoNavbar>
+            <span>Home</span>
+          </BotaoNavbar>
+
+          <BotaoNavbar>
+            <span>Novo Vídeo</span>
+          </BotaoNavbar>
+        </WrapperBotoes>
+      </nav>
+    </Header>
+  )
+}
+
+export default NavBar
